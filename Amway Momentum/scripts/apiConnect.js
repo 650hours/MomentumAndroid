7caa7edcc51d71860f1acdaeed0f346c7a2caaa3
Stewart Boutcher <ukbluehand@gmail.com>
2013-10-22 07:36:07 -0500

    White background in place throughout. Some styling starting to come through. Agenda working. Workshop listing working.

--- Amway Momentum/scripts/apiConnect.js
+++ Amway Momentum/scripts/apiConnect.js
@@ -1,27 +1,49 @@
+// Load the agenda
+function loadAgenda() {
+	$.ajax({
+	url: 'http://amway.650h.co.uk/index/default/getAgenda',
+	error: function() {
+		$("#resultBlock").html('Sorry, a connection problem occured, please try again.');	
+    },
+	cache: false}).done(function(data) {
+		$("#agendaTitle").html('<h1>' + data.agendaTitle + '</h1>');
+		$("#agendaWelcome").html(data.agendaText);
+	})
+}
+
 
 // Load workshop list
-function loadWorkShopList() {
+function loadWorkshopList() {
 	
 	var userWorkshopList = '';
-	var otherWorkshopList = '';
+	var otherWorkshopList = '<ul class="topcoat-list__container">';
 
 	$.ajax({
 	url: 'http://amway.650h.co.uk/index/default/getWorkshopsList',
-	error: function(){
-		$("#resultBlock").html('Sorry, we were unable to log you in, please try again.');	
+	error: function() {
+		$("#resultBlock").html('Sorry, a connection problem occured, please try again.');	
     },
 	cache: false}).done(function(data) {
-		alert('done');
+		
 		$.each(data, function(i,item) {
 			if(item.userIsAttending == 1) {
-				userWorkshopList = userWorkshopList + '<h2>' + item.workshopTitle + '<h2>';
+				userWorkshopList = userWorkshopList + '<a href="#tabstrip-workshop?wid='+item.workshopId+'"><li class="topcoat-list__item">' + item.workshopTitle + '</li></a>';
 			} else {
-				otherWorkshopList = otherWorkshopList + '<h2>' + item.workshopTitle + '<h2>';
+				otherWorkshopList = otherWorkshopList + '<li class="topcoat-list__item">' + item.workshopTitle + '</li>';
             }
 		})
-		alert(userWorkshopList);
-		alert(otherWorkshopList);
+		
+		otherWorkshopList = otherWorkshopList + '</ul>';
+		
 		$("#userWorkshopList").html(userWorkshopList);
 		$("#otherWorkshopList").html(otherWorkshopList);
 	});
+}
+
+
+// Load an individual workshop
+function loadWorkshop(e) {
+	
+	var workshopId = e.view.params.wid;
+	alert(workshopId);
 }
